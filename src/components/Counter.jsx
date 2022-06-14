df7862fbeec47cb8654d3479ee3ebeb8e98ba3d5
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-06-14 19:02:21 +0530

    feat: Store added
    Subscription added and counter defined

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+    const counter = useSelector(state => state.counter)
 
-    const toggleHandler = () =>{}
-    
+    const toggleHandler = () => { }
+
     return (
-        <>
-            <main className={classes.counter}>
-                <h1>Redux Counter</h1>
-                <div className={classes.value}> --- COUNTER VALUE ---</div>
-                <button onClick={toggleHandler}> Toggle Counter</button>
-            </main>
-        </>
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+            <button onClick={toggleHandler}> Toggle Counter</button>
+        </main>
     )
 }
 
